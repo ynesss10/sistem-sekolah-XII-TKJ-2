7ae19ca672bb9855cd5fb1594d2cc80fdf57d43c
ynesss10 <yns10109@gmail.com>
2026-07-26 14:52:04 +0700

    Implementasi MajorController

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -11,7 +11,7 @@ class MajorController extends Controller
      */
     public function index()
     {
-        //
+        return "Ini adalah halaman daftar jurusan";
     }
 
     /**
@@ -19,7 +19,7 @@ class MajorController extends Controller
      */
     public function create()
     {
-        //
+        return "Ini adalah halaman tambah jurusan";
     }
 
     /**
@@ -27,7 +27,7 @@ class MajorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return "Menambah data jurusan baru";
     }
 
     /**
@@ -35,7 +35,7 @@ class MajorController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return "Ini adalah halaman detail jurusan dengan ID: {$id}";
     }
 
     /**
@@ -43,7 +43,7 @@ class MajorController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return "Ini adalah halaman edit jurusan dengan ID: {$id}";
     }
 
     /**
@@ -51,7 +51,7 @@ class MajorController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        return "Mengubah data jurusan dengan ID: {$id}";
     }
 
     /**
@@ -59,6 +59,6 @@ class MajorController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        return "Menghapus data jurusan dengan ID: {$id}";
     }
 }
